75cff55ae760ca2581d51c1f42f095b63d2d0957
Adin Had <adinhadzovic1993@gmail.com>
2021-03-18 00:49:56 +0100

    commit

---
+++ ex01/src/index.js
@@ -0,0 +1,15 @@
+// Only change code below this line
+var myPet = {
+    species: "Dog",
+    name: "Pujdo",
+    legs: 4,
+    friends: ["Max", "Vucko"]
+}
+
+function myFunction(myPet) {
+    return myPet;
+}
+
+// Only change code above this line
+console.log(myFunction(myPet)); // Change this line
+module.exports = { myPet, myFunction };
